8bc6eb2c489aa3c2f9d85badf89d5b9809708d2c
Ivan <menkov_99@mail.ru>
2019-10-11 23:33:00 +0300

    create

---
+++ src/main/java/store/controllers/HelloWorldController.java
@@ -0,0 +1,14 @@
+package store.controllers;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping("/hello")
+    public String helloWorld(){
+        return "hello kusok pidarasiny";
+    }
+}
